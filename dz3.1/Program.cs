dec540aaaa614d69e73b055f9cacf780a35d02d2
Koshara <ukracent2009@gmail.com>
2019-09-26 05:15:03 +0300

    first comm

---
+++ dz3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int[] arrCopy = new int[10];
+
+
+            int countMin = arr.Length;
+            int countMax = arrCopy.Length;
+            if (countMin > countMax)
+            {
+                (countMin, countMax) = (countMax, countMin);
+                (arr, arrCopy) = (arrCopy, arr);
+            }
+            int index = (countMax - countMin) / 2;
+            Array.Copy(arr, 0, arrCopy, index, countMin);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arrCopy[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
